65601eaa2229b3469ba0968ec85dc98c3bded548
Andreas <>
2022-03-23 13:22:38 +0100

    define.h new

---
+++ src/define.h
@@ -0,0 +1,11 @@
+
+
+#define MQTT_SERVER         "192.168.178.21"
+#define MQTT_PORT           "1883"
+
+#define WIFI_AP_NAME        "Wifi_EspAP"
+#define MQTT_CLIENT_NAME    "Wifi_MQTT"
+#define OTA_CLIENT_NAME     "Wifi_OTA"
+
+
+
