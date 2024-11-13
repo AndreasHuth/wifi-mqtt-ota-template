4c90dfd3677fc654ae4723ac042d99a55c97076d
Andreas <68425537+AndreasHuth@users.noreply.github.com>
2024-11-13 20:30:25 +0100

    update define.h

--- src/define.h
+++ src/define.h
@@ -3,15 +3,13 @@
 #define MQTT_CLIENT_NAME    "Wifi_MQTT"
 #define OTA_CLIENT_NAME     "Wifi_OTA"
 
-
 // MQTT definition
 #define MQTT_SERVER         "192.168.0.192"
-#define MQTT_SERVER_TEST    "192.168.1.0"
 #define MQTT_PORT           "1883"
 
+//  Webpage
 #define DEVICE_NAME         "WEMOS D1 MINI - Testsetup"
 #define SW_BASE_VERSION     "V1.1"
-
 
 // PIN definition 
 #define PIN_LED_ex      13
